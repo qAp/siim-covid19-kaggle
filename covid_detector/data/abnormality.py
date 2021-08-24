2c95e8cac53ba211f8fdcb1edf930f039f335474
Jack Yu <jackchungchiehyu@gmail.com>
2021-08-25 00:12:33 +0800

    Torch Dataset and collate func for batch
    'Abnormality' dataset.

--- covid_detector/data/abnormality.py
+++ covid_detector/data/abnormality.py
@@ -1,62 +1,140 @@
-
-from pathlib import Path
-import pytorch_lightning as pl
-
 from pathlib import Path
 import argparse
+
+import numpy as np
 import pandas as pd
+import torch
 import pytorch_lightning as pl
+
+import PIL
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+
+import matplotlib
+import matplotlib.pyplot as plt
 
 from covid_detector.util import *
 
 
 DIR_DATA = Path('/kaggle/input')
-STUDY_LEVEL_TARGETS_LONG = ['Negative for Pneumonia', 'Typical Appearance', 'Indeterminate Appearance', 
-                            'Atypical Appearance']
-STUDY_LEVEL_TARGETS_SHORT = ["negative", "typical", "indeterminate", "atypical"]
 JPG_IMG_SIZE = 256
+CAT_NAMES = ['opacity', 'negative', 'typical', 'indeterminate', 'atypical']
+MAX_NUM_INSTANCES = 100
+
+BATCH_SIZE = 16
+NUM_WORKERS = 4
 
 
+class AbnormalityDataset(torch.utils.data.Dataset):
+    '''
+    Object detection dataset for 5 abnormality classes:
+    'opacity', 'negative', 'typical', 'indeterminate', and 'atypical'.
+    '''
 
-def _prepare_train_examples(dir_siim, dir_jpg):
-    df_jpg = pd.read_csv(dir_jpg / 'meta.csv')
-    df_jpg['pth_jpg'] = df_jpg.apply(lambda r: dir_jpg / r.split / f'{r.image_id}.jpg', axis=1)
+    def __init__(self, df, transform=None):
+        self.df = df
+        self._transform = transform
 
-    df_study = pd.read_csv(dir_siim / 'train_study_level.csv')
-    df_study['id'] = df_study.id.apply(lambda id: id.replace('_study', ''))
-    df_study.rename({'id': 'study_id'}, axis=1, inplace=True)
+    def __len__(self):
+        return len(df)
 
-    df_image = pd.read_csv(dir_siim / 'train_image_level.csv')
-    df_image['id'] = df_image.id.apply(lambda id: id.replace('_image', ''))
-    df_image.drop('label', axis=1, inplace=True)
-    df_image.rename({'id': 'image_id', 'StudyInstanceUID': 'study_id'}, axis=1, inplace=True)
+    def __getitem__(self, idx):
+        r = df.iloc[idx]
 
-    df = pd.merge(df_image, df_study, left_on='study_id', right_on='study_id', how='inner')
-    df = df[['image_id', 'study_id', 'boxes'] + STUDY_LEVEL_TARGETS_LONG]
+        img = PIL.Image.open(r.pth_jpg).convert('RGB')
+        img_width, img_height = img.size
+        img = np.array(img)
 
-    df = pd.merge(df, df_jpg, left_on='image_id', right_on='image_id', how='inner')
-    return df
+        bboxes = []
+        cls = []
+
+        if isinstance(r.boxes, list):
+            for d_box in r.boxes:
+                x, y = d_box['x'], d_box['y']
+                width, height = d_box['width'], d_box['height']
+                bbox = [y, x, y + height, x + width]
+                bboxes.append(bbox)
+                cls.append(CAT_NAMES.index('opacity'))
+
+        for i, label_long in enumerate(STUDY_LEVEL_TARGETS_LONG):
+            if r[label_long] == 1:
+                bboxes.append([0, 0, img_height, img_width])
+                short_label = STUDY_LEVEL_TARGETS_SHORT[i]
+                cls.append(CAT_NAMES.index(short_label))
+
+        if bboxes:
+            bboxes = np.array(bboxes, ndmin=2, dtype=np.float32)
+            cls = np.array(cls, dtype=np.int64)
+        else:
+            bboxes = np.zeros((0, 4), dtype=np.float32)
+            cls = np.array([], dtype=np.int64)
+
+        target = {'img_idx': idx,
+                  'img_size': (img_width, img_height),
+                  'bbox': bboxes,
+                  'cls': cls}
+
+        if self.transform:
+            transformed = self.transform(
+                image=img, bbox=target['bbox'], cls=target['cls'])
+            img = transformed['image']
+            target = {'img_idx': target['img_idx'],
+                      'img_size': target['img_size'],
+                      'bbox': transformed['bbox'],
+                      'cls': transformed['cls']}
+
+        return img, target
+
+    @property
+    def transform(self):
+        return self._transform
+
+    @transform.setter
+    def transform(self, t):
+        self._transform = t
+
+    @staticmethod
+    def show(img, target):
+        fig, ax = plt.subplots(figsize=(10, 10))
+
+        img_ = img.copy()
+
+        for cls, bbox in zip(target['cls'], target['bbox']):
+            y0, x0, y1, x1 = bbox.astype(np.int64)
+
+            cv2.rectangle(img_, (x0, y0), (x1, y1), (255, 0, 0), 5)
+
+            cv2.putText(img_, CAT_NAMES[cls], org=(x0 + 5, y0 + 20),
+                        fontFace=cv2.FONT_HERSHEY_PLAIN,
+                        fontScale=2,
+                        color=(255, 0, 0))
+
+        ax.imshow(img_)
+
+        return fig, ax
 
 
-def _prepare_test_examples(dir_siim, dir_jpg):
-    df_jpg = pd.read_csv(dir_jpg / 'meta.csv')
-    df_jpg['pth_jpg'] = df_jpg.apply(lambda r: dir_jpg / r.split / f'{r.image_id}.jpg', axis=1)
+def abnormality_collate_fn(batch):
+    batch_size = len(batch)
+    image_size = 512
 
-    df_test = pd.read_csv(dir_siim / 'sample_submission.csv')
-    is_study = df_test.id.apply(lambda id: id.endswith('_study'))
-    study_ids = df_test[is_study].id.apply(lambda id: id.replace('_study', '')).values
+    img_b = torch.zeros((batch_size, 3, image_size, image_size), dtype=torch.float32)
+    bbox_b = torch.full((batch_size, MAX_NUM_INSTANCES, 4), -1, dtype=torch.float32)
+    cls_b = torch.full((batch_size, MAX_NUM_INSTANCES), -1, dtype=torch.int64)
 
-    study_id_list = []
-    img_id_list = []
-    for study_id in study_ids:
-        for dicom_pth in (dir_siim / 'test' / study_id).rglob('*.dcm'):
-            img_id = dicom_pth.stem
-            study_id_list.append(study_id)
-            img_id_list.append(img_id)
+    for i in range(batch_size):
+        img, target = batch[i]
+        bbox, cls = target['bbox'], target['cls']
+        num_instances = len(bbox)
+        num_elem = min(num_instances, MAX_NUM_INSTANCES)
 
-    df = pd.DataFrame({'image_id': img_id_list, 'study_id': study_id_list})
-    df = pd.merge(df, df_jpg, left_on='image_id', right_on='image_id', how='inner')
-    return df
+        img_b[i] = img
+        bbox_b[i, :num_elem] = torch.from_numpy(target['bbox'][:num_elem])
+        cls_b[i, :num_elem] = torch.from_numpy(target['cls'][:num_elem])
+
+    target_b = {'bbox': bbox_b, 'cls': cls_b}
+
+    return img_b, target_b
 
 
 class Abnormality(pl.LightningDataModule):
